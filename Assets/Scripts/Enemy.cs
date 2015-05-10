6e40705572df086004c23d79d0c6720e17a3e637
Johan <jdequeant@gmail.com>
2015-05-10 05:55:20 +0200

    treztr

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,14 +11,13 @@ public class Enemy : MonoBehaviour
 	public GameObject hundredPointsUI;	// A prefab of 100 that appears when the enemy dies.
 	public float deathSpinMin = -100f;			// A value to give the minimum amount of Torque when dying
 	public float deathSpinMax = 100f;			// A value to give the maximum amount of Torque when dying
-
-
-	private SpriteRenderer ren;			// Reference to the sprite renderer.
-	private Transform frontCheck;		// Reference to the position of the gameobject used for checking if something is in front.
-	private bool dead = false;			// Whether or not the enemy is dead.
-	private Score score;				// Reference to the Score script.
-
-
+	
+	
+	protected SpriteRenderer ren;			// Reference to the sprite renderer.
+	protected Transform frontCheck;		// Reference to the position of the gameobject used for checking if something is in front.
+	protected bool dead = false;			// Whether or not the enemy is dead.
+	protected Score score;				// Reference to the Score script.
+	
 	
 	void Awake()
 	{
@@ -27,12 +26,13 @@ public class Enemy : MonoBehaviour
 		frontCheck = transform.Find("frontCheck").transform;
 		score = GameObject.Find("Score").GetComponent<Score>();
 	}
-
+	
 	void FixedUpdate ()
 	{
+		ren = transform.Find("body").GetComponent<SpriteRenderer>();
 		// Create an array of all the colliders in front of the enemy.
 		Collider2D[] frontHits = Physics2D.OverlapPointAll(frontCheck.position, 1);
-
+		
 		// Check each of the colliders.
 		foreach(Collider2D c in frontHits)
 		{
@@ -44,16 +44,15 @@ public class Enemy : MonoBehaviour
 				break;
 			}
 		}
-
+		
 		// Set the enemy's velocity to moveSpeed in the x direction.
-
-			GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x-1 * (moveSpeed*2), GetComponent<Rigidbody2D>().velocity.y);
-			
+		GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x - 1 * ( moveSpeed * 2 ), GetComponent<Rigidbody2D>().velocity.y);	
+		
 		// If the enemy has one hit point left and has a damagedEnemy sprite...
 		if(HP == 1 && damagedEnemy != null)
 			// ... set the sprite renderer's sprite to be the damagedEnemy sprite.
 			ren.sprite = damagedEnemy;
-			
+		
 		// If the enemy has zero or fewer hit points and isn't dead yet...
 		if(HP <= 0 && !dead)
 			// ... call the death function.
@@ -66,52 +65,52 @@ public class Enemy : MonoBehaviour
 		HP--;
 	}
 	
-	void Death()
+	protected virtual void Death()
 	{
 		// Find all of the sprite renderers on this object and it's children.
 		SpriteRenderer[] otherRenderers = GetComponentsInChildren<SpriteRenderer>();
-
+		
 		// Disable all of them sprite renderers.
 		foreach(SpriteRenderer s in otherRenderers)
 		{
 			s.enabled = false;
 		}
-
+		
 		// Re-enable the main sprite renderer and set it's sprite to the deadEnemy sprite.
 		ren.enabled = true;
 		ren.sprite = deadEnemy;
-
+		
 		// Increase the score by 100 points
 		score.score += 100;
-
+		
 		// Set dead to true.
 		dead = true;
-
+		
 		// Allow the enemy to rotate and spin it by adding a torque.
 		GetComponent<Rigidbody2D>().fixedAngle = false;
 		GetComponent<Rigidbody2D>().AddTorque(Random.Range(deathSpinMin,deathSpinMax));
-
+		
 		// Find all of the colliders on the gameobject and set them all to be triggers.
 		Collider2D[] cols = GetComponents<Collider2D>();
 		foreach(Collider2D c in cols)
 		{
 			c.isTrigger = true;
 		}
-
+		
 		// Play a random audioclip from the deathClips array.
 		int i = Random.Range(0, deathClips.Length);
 		AudioSource.PlayClipAtPoint(deathClips[i], transform.position);
-
+		
 		// Create a vector that is just above the enemy.
 		Vector3 scorePos;
 		scorePos = transform.position;
 		scorePos.y += 1.5f;
-
+		
 		// Instantiate the 100 points prefab at this point.
 		Instantiate(hundredPointsUI, scorePos, Quaternion.identity);
 	}
-
-
+	
+	
 	public void Flip()
 	{
 		// Multiply the x component of localScale by -1.
