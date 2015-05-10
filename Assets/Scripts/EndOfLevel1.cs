83d3808c5b74e9c2893741dcf0c50f97027c5e91
Johan <jdequeant@gmail.com>
2015-05-10 11:02:20 +0200

    yo

---
+++ Assets/Scripts/EndOfLevel1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndOfLevel1 : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		Debug.Log ("passe3");
+		Application.LoadLevel("Boss1");
+	}
+}
